ecf5868dd8218e8726761fe7f9c97d304005589b
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-14 12:21:03 +0200

    objet de la classe order

--- model/order.php
+++ model/order.php
@@ -9,6 +9,17 @@ class order {
     public $status;
 
 }
+// je crée une instance de la classe order
+// je lui passe en paramètre le produit, la quantité, date de création et le status 
+$order = new order();
 
+$order->product = "Teeshirt Mario";
+$order->quantity = 2;
+$order->createdAt = new DateTime("now");
+$order->status = "CART";
 
+$order2 = new order();
+$order2->product = "Teeshirt Mario 2";
+$order2->quantity = 3;
+$order2->createdAt = new DateTime("now");
 
